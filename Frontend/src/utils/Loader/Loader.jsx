59a3e5a312eede3bae2c3e33c79f93a32f4e29bd
SGO34 <steve.garneri@hotmail.fr>
2023-05-11 10:11:50 +0200

    Upgraded Loader

--- Frontend/src/utils/Loader/Loader.jsx
+++ Frontend/src/utils/Loader/Loader.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWineBottle } from "@fortawesome/free-solid-svg-icons";
 
 function Loader() {
-  return <Loading />;
+  return (
+    <Loading>
+      <FontAwesomeIcon
+        icon={faWineBottle}
+        rotation={90}
+        size="2xl"
+      />
+    </Loading>
+  );
 }
 
 export default Loader;
@@ -22,7 +32,10 @@ const Loading = styled.div`
   border: 10px solid #ac1c35;
   border-bottom-color: transparent;
   border-radius: 50%;
-  animation: ${rotate} 0.5s infinite linear;
+  animation: ${rotate} 1s infinite linear;
   height: 50px;
   width: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
